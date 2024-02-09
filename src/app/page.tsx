80a6163ebf57cbeef41c63cfad8af9980ffdd51e
MrYious <rosariomark37@gmail.com>
2024-02-09 14:03:06 +0800

    update metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@
 
 export default function Home() {
   return (
-    <></>
+    <main className="flex items-center justify-center min-h-screen text-4xl">
+      <div className="font-sans">Sample</div>
+      <div>Sample</div>
+    </main>
   );
 }
